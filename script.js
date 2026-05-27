92fc5158b0701be3daf92019a48cc1971a0648ae
jessicakangas <jessica6990@icloud.com>
2026-05-27 16:58:00 +0200

    Rettet/ændret struktur i første del af JS

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 /*COLORSELECTOR - INTERAKTIVITET*/
 
-/*Henter de fire farvebjælker og billedet af højtaleren */
+/*Henter de fire farve containere og billedet af højtaleren */
+
 const colorBars = document.querySelectorAll ('.color-bar');
 const mainProductImg = document.querySelector ('#speakerImage');
 
-/* Fortæller koden, at den skal tage farvebjælkerne én efter én og sætte en lyttepost på hver enkelt */
+
 colorBars.forEach(bar => {
     bar.addEventListener('click', () => {
 
-        /* Fjerner den mørke ramme (.active) fra alle de andre bjælker først */
+        /* Fjerner den mørke ramme (.active) fra alle de andre containere */
         colorBars.forEach(b => b.classList.remove('active'));
 
-        /* Giv den mørke ramme til den bjælke, man klikker på */
+        /* Mørk ramme ved klik på container*/
         bar.classList.add('active');
 
-        /* Henter billedstien fra data-color på den klikkede bjælke */
+        /* Henter billedstien fra data-color-attributten ved klik */
         const newImgSrc = bar.getAttribute('data-color');
         
-        /* Skifter det store (første) billede ud til den nye farve */
+        /* Skifter det store (første) billede ud til nyt billede */
         if (newImgSrc) {
             mainProductImg.src = newImgSrc;
         }
     });
 });
+
